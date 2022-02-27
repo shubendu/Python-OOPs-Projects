f57232061b7298af6cb46537faeb6195b224eb4c
shubendu <biswasshubendu4@gmail.com>
2022-02-28 02:42:15 +0530

    updating app6

---
+++ App-6-Project-Calorie-Webapp/calorie.py
@@ -0,0 +1,19 @@
+from temperature import Temperature
+
+class Calorie:
+    """Represent amount of calories calculated with BMR = 10*weight + 6.25*height - 5*age + 5 - 10*temperature
+    """
+    def __init__(self, weight, height, age, temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
+        result =  10 * self.weight + 6.25 * self.height - 5 * self.age + 5 - 10 *self.temperature
+        return result
+
+if __name__ =="__main__":
+    temperature = Temperature(city="delhi", country = "india").get()
+    calorie = Calorie(temperature=temperature, weight=60, height=130, age=25)
+    print(calorie.calculate())
---
+++ App-6-Project-Calorie-Webapp/main.py
@@ -0,0 +1,3 @@
+from flask.views import MethodView
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template
